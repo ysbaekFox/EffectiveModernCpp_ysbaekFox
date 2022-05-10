05926ed53d8f29e37088a93bc9271d19592af351
ysbaekFox <ysbaekFox@gmail.com>
2022-05-10 23:01:23 +0900

    :white_check_mark: std::initializer_list & ctor test

--- Chapter_3_Moving_to_Modern_Cpp/Item_7/Distinguish_brace_parenthese.cpp
+++ Chapter_3_Moving_to_Modern_Cpp/Item_7/Distinguish_brace_parenthese.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 struct Widget1
 {
@@ -85,6 +86,70 @@ FloatWidget::operator float() const
 	return 0.0;
 }
 
+class BestMatchWidget
+{
+public:
+	BestMatchWidget(int i, bool b);
+	BestMatchWidget(int i, double b);
+	BestMatchWidget(std::initializer_list<bool> il);
+};
+
+BestMatchWidget::BestMatchWidget(int i, bool b)
+{
+	std::cout << "Widget(int i, bool b)" << std::endl;
+}
+
+BestMatchWidget::BestMatchWidget(int i, double b)
+{
+	std::cout << "Widget(int i, double b)" << std::endl;
+}
+
+BestMatchWidget::BestMatchWidget(std::initializer_list<bool> il)
+{
+	std::cout << "Widget(std::initializer_list<bool> il)" << std::endl;
+}
+
+class NoWayWidget
+{
+public:
+	NoWayWidget(int i, bool b);
+	NoWayWidget(int i, double b);
+	NoWayWidget(std::initializer_list<std::string> il);
+};
+
+NoWayWidget::NoWayWidget(int i, bool b)
+{
+	std::cout << "Widget(int i, bool b)" << std::endl;
+}
+
+NoWayWidget::NoWayWidget(int i, double b)
+{
+	std::cout << "Widget(int i, double b)" << std::endl;
+}
+
+NoWayWidget::NoWayWidget(std::initializer_list<std::string> il)
+{
+	std::cout << "Widget(std::initializer_list<std::string> il)" << std::endl;
+}
+
+
+class EdgeCaseWidget
+{
+public:
+	EdgeCaseWidget();
+	EdgeCaseWidget(std::initializer_list<int> il);
+};
+
+EdgeCaseWidget::EdgeCaseWidget()
+{
+	std::cout << "Widget()" << std::endl;
+}
+
+EdgeCaseWidget::EdgeCaseWidget(std::initializer_list<int> il)
+{
+	std::cout << "Widget(std::initializer_list<int> il)" << std::endl;
+}
+
 int main()
 {
 	{
@@ -130,6 +195,53 @@ int main()
 		FloatWidget w6{ w4 };				// Widget(std::initializer_list<long double> il)
 		FloatWidget w7( std::move(w4) );	// call move ctor
 		FloatWidget w8{ std::move(w4) };	// Widget(std::initializer_list<long double> il)
+
+		std::cout << std::endl;
+		std::cout << std::endl;
+	}
+
+	{
+		/*
+		* braces { } 는 std::initializer_list<bool>과 매우 강력하게 연결 되어있습니다.
+		* 심지어 알맞은 matching이 아니더라도 연결되어 컴파일 에러가 발생합니다.
+		* 왜냐하면 std::initializer_list<bool>로 인해 narrowing conversion이 발생하게 될텐데,
+		* C++ 규칙상, braces { } 안에서는 narrowing conversion이 금지되어 있기 때문입니다.
+		*/
+
+		// BestMatchWidget w{ 10, 5.0 };
+
+		NoWayWidget w1( 10, true ); // Widget(int i, bool b)
+		NoWayWidget w2{ 10, true }; // Widget(int i, bool b)
+		NoWayWidget w3( 10, 5.0 );  // Widget(int i, double b)
+		NoWayWidget w4{ 10, 5.0 };  // Widget(int i, double b)
+		
+		std::cout << std::endl;
+		std::cout << std::endl;
+	}
+
+	{
+		/*
+		* 대부분의 case를 다뤘지만, Edge Case가 있습니다.
+		* default 생성자와 std::initializer_list를 함께 쓰는 경우 입니다.
+		* 이런 경우 most vexing parse가 발생할 수 있습니다.
+		*/
+
+		EdgeCaseWidget w1;		// Widget()
+		EdgeCaseWidget w2{};	// Widget()
+		EdgeCaseWidget w3();	// most vexing parse!!
+		EdgeCaseWidget w4({});	// Widget(std::initializer_list<int> il)
+		EdgeCaseWidget w5{{}};	// Widget(std::initializer_list<int> il)
+	}
+
+	{
+		/*
+		* 지금까지 알아본 이상한 규칙들이 얼마나 실제 코딩에 있어서 중요한 정보일까요?
+		* 당신이 생각하는 것보다 더 중요합니다. 왜냐하면 영향이 가는 클래스들 중 하나가
+		* 바로 std::vector 이기 때문이죠.
+		*/
+		
+		std::vector<int> v1(10, 20);	// { 20, 20, ... 20 } 10개
+		std::vector<int> v2{ 10, 20 };	// { 10, 20 } 2개		
 	}
 
 	return 0;
